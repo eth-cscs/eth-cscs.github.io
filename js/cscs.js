1a72193612bdba994ef25cfd44d671461069057e
Victor Holanda Rusu <victor.holanda@cscs.ch>
2017-03-23 13:47:20 +0100

    Prepending the domain to the navbar

--- js/cscs.js
+++ js/cscs.js
@@ -4,6 +4,8 @@
  */
 var __cscsMarkDown = "";
 
+// this function reads file contents based on http request
+// it should be used by all markdown based pages
 function cscs_read_file_contents(filename, callback)
 {
   var rawFile = new XMLHttpRequest();
@@ -12,18 +14,21 @@ function cscs_read_file_contents(filename, callback)
   var __fileContent = "";
   rawFile.onreadystatechange = function ()
   {
-      if(rawFile.readyState === 4) {
-          if(rawFile.status === 200 || rawFile.status == 0) {
-              __fileContent = rawFile.responseText;
-              if(typeof callback == 'function'){
-                callback(__fileContent);
-              }
-          }
+    if(rawFile.readyState === 4) {
+      if(rawFile.status === 200 || rawFile.status == 0) {
+        __fileContent = rawFile.responseText;
+        if(typeof callback == 'function'){
+          callback(__fileContent);
+        }
       }
+    }
   }
   rawFile.send(null);
 }
 
+// this function gets the base html and parses the sidebar using the same markdown used by remark.js
+// before changing the markdown one needs to verify if it is compatible with the
+// presentation mode
 function cscs_setup_site_content(navbarfile, sidebarfile) {
 
   cscs_read_file_contents(navbarfile, function __populate_site_content(argument) {
@@ -48,6 +53,7 @@ function cscs_setup_site_content(navbarfile, sidebarfile) {
 
   });
 
+  // presentation mode is hidden if remark.js is not included
   try {
     if(remark != null) {
       $('#start-cscs-presenter-mode').show();
@@ -63,8 +69,12 @@ function cscs_setup_site_content(navbarfile, sidebarfile) {
   presenterMode.onclick = __cscs_show_in_presenter_mode;
 
   __cscs_email_protector();
+  __cscs_prepend_domain_to_links();
 }
 
+// this function parses the markdown using the same markdown used by remark.js
+// before changing the markdown one needs to verify if it is compatible with the
+// presentation mode
 function cscs_setup_markdown_page_content(markdownFile)
 {
   __cscsMarkDown = markdownFile;
@@ -85,30 +95,6 @@ function cscs_setup_markdown_page_content(markdownFile)
       document.getElementById("cscs-markdown-content").innerHTML = content;
     });
   });
-  __cscs_mouseover_link();
-  __cscs_create_toc();
-  __cscs_change_table_layout();
-  __cscs_highlight_code();
-}
-
-function cscs_setup_index_page_content(newsfile)
-{
-  cscs_read_file_contents(newsfile, function __populate_site_content(argument) {
-    // converting markdown to html
-    marked(argument, function (err, content) {
-      if (err) throw err;
-      document.getElementById("cscs-markdown-content").innerHTML = content;
-    });
-  });
-
-  cscs_read_file_contents("https://raw.githubusercontent.com/eth-cscs/production/master/jenkins-builds/6.0.UP02-2016.11-gpu", function __populate_site_content(argument) {
-    document.getElementById("cscs-markdown-content").innerHTML += '<h2>Daint GPU partition</h2><pre>' + argument + '</pre>';
-  });
-
-  cscs_read_file_contents("https://raw.githubusercontent.com/eth-cscs/production/master/jenkins-builds/6.0.UP02-2016.11-mc", function __populate_site_content(argument) {
-    document.getElementById("cscs-markdown-content").innerHTML += '<h2>Daint MC partition</h2><pre>' + argument + '</pre>';
-  });
-
   __cscs_mouseover_link();
   __cscs_create_toc();
   __cscs_change_table_layout();
@@ -145,6 +131,8 @@ function cscs_setup_index_page_content(newsfile)
 //   });
 // }
 
+// this function starts the presentation mode
+// this mode destroys completely the page. so we refresh the page if the exit button is clicked
 function __cscs_show_in_presenter_mode() {
   document.getElementById("cscs-body-container").innerHTML = null;
 
@@ -169,35 +157,39 @@ function __cscs_show_in_presenter_mode() {
   }
 }
 
-
+// protects cscs' e-mail from robots
 function __cscs_email_protector() {
   // $("#cscs-email-protector").prepend('<a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;%69%6E%66%6F%40%63%73%63%73%2E%63%68">Contact CSCS</a>');
   $("#cscs-email-protector").prepend('<a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#104;&#101;&#108;&#112;&#064;&#099;&#115;&#099;&#115;&#046;&#099;&#104;">Contact us</a>');
 }
 
+// this function wraps the markdown headers with a link
+// it does it on mouse hover. But it is not really needed
+// We could do it immediately without the need of hovering
 function __cscs_mouseover_link() {
 
   $('#cscs-markdown-content').children("h1, h2").each(function(index, element) {
     $(element).hover(
-        function() {
-            $(this).wrap(function() {
-                return "<a id='" + $(this).attr('id') + "' href='#" + $( this ).attr('id') + "'></a>";
-            });
-        },
-        function() {
-            $(this).unwrap();
-        }
+      function() {
+        $(this).wrap(function() {
+          return "<a id='" + $(this).attr('id') + "' href='#" + $( this ).attr('id') + "'></a>";
+        });
+      },
+      function() {
+        $(this).unwrap();
+      }
     );
   });
 }
 
+// this function creates the toc based on the main markdown content
 function __cscs_create_toc() {
   // TOC creation
   $('#toc').TOC();
   $('#toc').append('<a class="back-to-top" href="#">Back to top</a>');
   // Sidenav affixing
   setTimeout(function () {
-  $('#toc').affix({
+    $('#toc').affix({
       offset: {
         // top: $('.cscs-global-nav').outerHeight(),
         top: function () {
@@ -208,7 +200,7 @@ function __cscs_create_toc() {
         }
 
       }
-  })}, 100);
+    })}, 100);
   var $window = $(window);
   var $body   = $(document.body);
 
@@ -220,6 +212,7 @@ function __cscs_create_toc() {
     $body.scrollspy('refresh');
   });
 
+  // counting the number of h1 levels in the toc
   var tocsize = 0;
   $('#toc').children("ul").each(function(index, element) {
     var size = $(this).children("li").length;
@@ -228,6 +221,7 @@ function __cscs_create_toc() {
     }
   });
 
+  // expanding toc when there is only one h1
   if(tocsize == 1) {
     $("head").append("<style> nav[data-toggle='toc'] .nav .nav { display: block; } </style>");
   }
@@ -235,10 +229,10 @@ function __cscs_create_toc() {
 
 // This function destroys the remark presentation and restores the CSCS website
 function __cscs_exit_presentation_mode() {
-    if(document.location.domain == null)
-      window.location.assign(document.location.pathname);
-    else
-      window.location.assign(document.location = document.location.domain+document.location.pathname);
+  if(document.location.domain == null)
+    window.location.assign(document.location.pathname);
+  else
+    window.location.assign(document.location = document.location.domain+document.location.pathname);
 }
 
 // this function changes the layout of table inside the main side markdown area
@@ -251,20 +245,33 @@ function __cscs_change_table_layout() {
   });
 }
 
+// this function adds code highlight
 function __cscs_highlight_code() {
   try {
     if(hljs != null) {
-        $("head").append('<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.10.0/styles/default.min.css">');
-        $("head").append("<style> .hljs { background: none; } </style>");
-        $('pre code').each(function(i, block) {
-            hljs.highlightBlock(block);
-        });
+      $("head").append('<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.10.0/styles/default.min.css">');
+      $("head").append("<style> .hljs { background: none; } </style>");
+      $('pre code').each(function(i, block) {
+        hljs.highlightBlock(block);
+      });
     }
   } catch(msg) {
     // no need to catch
   }
 }
 
+function __cscs_prepend_domain_to_links() {
+  var domain = "";
 
+  if(document.location.domain != null) {
+    domain = document.location.domain;
+  }
+  domain += "/";
 
-
+  // prepending cscs domain
+  $('.cscs-prepend-domain').each(function(index, element) {
+    $(element).attr('href',function(i,v) {
+      return domain + v;
+    });
+  });
+}
